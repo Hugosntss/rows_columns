fcd532283989d1764b488d6e99f088f39708e23c
Hugo <hugos@Hugos-MacBook-Air.local>
2024-09-15 21:49:04 -0700

    table program added

---
+++ columns_rows.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    cout << left << setw(15) << "Course" << setw(15) << "Students" << endl; //header using iomanip libraly 
+
+    cout << left << setw(15) << "C++" << right << setw(5) << 100 << endl; // the setw(10) is to make a space between numbers
+    cout << left << setw(15) << "JavaScript" << right << setw(5) << 50 << endl;
+
+    return 0;
+}
