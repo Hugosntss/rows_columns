e22f996a4d2d53f444fc946bb2e27b2907add645
Hugo <hugos@Hugos-MacBook-Air.local>
2024-09-15 21:50:19 -0700

    comment added

--- columns_rows.cc
+++ columns_rows.cc
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <iomanip>
-
 using namespace std;
+
+/*
+
+This program has the task to show how many students 
+the courses JavaScript and C++ have in total
+
+*/
+
+
 
 int main() {
     cout << left << setw(15) << "Course" << setw(15) << "Students" << endl; //header using iomanip libraly 
